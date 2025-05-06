418a2329a765e4cf1cc9308c5ff983db45257ad8
Joao-Pedro-Queiroz <jpqv0105@gmail.com>
2025-05-06 14:23:14 -0300

    importação necessária

--- projeto_af/src/main.jsx
+++ projeto_af/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 createRoot(document.getElementById('root')).render(
   <Auth0Provider
